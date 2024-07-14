b19f4db5fe25788bbb3c7bb24e63ac26994904af
Jerson Cruz <131726867+jersonscruz@users.noreply.github.com>
2024-07-13 19:47:38 -0700

    Update SQLFileEmployeesDatabase.sql

--- SQLFileEmployeesDatabase.sql
+++ SQLFileEmployeesDatabase.sql
@@ -250,3 +250,223 @@ INNER JOIN cte_highest_salary c1 ON e.emp_no = c1.emp_no
 CROSS JOIN cte_avg_salary c2
 # Filter for the male gender
 WHERE e.gender = 'M';
+
+
+# 9) Obtain a table containing the employee number, first name, last name, hire date, and the number of days between the employee's hire date and the current date.
+#    Filter for employees whose number of days between the current date and their hire date is less than 30 days. 
+#    Order the table by hire date, then employee number, both in ascending order.
+
+SELECT
+	emp_no,
+    	first_name,
+    	last_name,
+	hire_date,
+    	# Use DATEDIFF function to calculate the number of days between two dates
+    	DATEDIFF(day, current_date, hire_date) as date_difference
+FROM employees
+# Specify the condition for the number of days in between
+WHERE DATEDIFF(current_date, hire_date) < 30
+ORDER BY hire_date, emp_no ASC;
+
+
+# 10) Obtain a table containing the employee number, first_name, last_name, 
+#     the 5 most recent dates that an employee's salary as changed, 
+#     and the rolling average for those 5 most recent salary changes.
+#     Write two queries: one using a subquery, and another using a CTE (common table expression).
+
+
+# Subquery Solution
+SELECT
+	emp_no,
+    	first_name,
+    	last_name,
+	salary,
+	from_date,
+    	# Calculate the rolling average salaries by employee
+	AVG(salary) OVER(PARTITION BY emp_no ORDER BY from_date ASC) AS rolling_average_salary
+FROM
+	(SELECT
+		e.emp_no,
+        	e.first_name,
+        	e.last_name,
+		s.salary,
+		s.from_date,
+        	# Create a row number column in salary change date descending order
+		ROW_NUMBER() OVER(PARTITION BY e.emp_no ORDER BY s.from_date DESC) AS date_row_number
+	 FROM employees e
+	 INNER JOIN salaries s on e.emp_no = s.emp_no) a
+# Specify the 5 most recent salary change dates     
+WHERE date_row_number <= 5
+ORDER BY emp_no, from_date ASC;
+
+
+# CTE Solution
+WITH cte AS
+	(SELECT
+		e.emp_no,
+        	e.first_name,
+        	e.last_name,
+		s.salary,
+		s.from_date,
+        	# Create a row number column in salary change date descending order
+		ROW_NUMBER() OVER(PARTITION BY e.emp_no ORDER BY s.from_date DESC) AS date_row_number
+	 FROM employees e
+	 INNER JOIN salaries s on e.emp_no = s.emp_no)
+	
+SELECT
+	emp_no,
+    	first_name,
+    	last_name,
+	salary,
+	from_date,
+    	# Calculate the rolling average salaries by employee
+	AVG(salary) OVER(PARTITION BY emp_no ORDER BY from_date ASC) AS rolling_average_salary
+FROM cte
+# Specify the 5 most recent salary change dates
+WHERE date_row_number <= 5
+ORDER BY emp_no, from_date ASC;
+
+
+# 11) Create both a view and temporary table of employees whose last name falls between the letter 'A' and letter 'J' (inclusive)
+#	  along with the number of unique titles that the employee has held, given the condition that the employee has held more than one title.
+#	  The field list should contain:
+#	  - Full Name (this should be formatted as "Last Name, First Name")
+#	  - Tile Count
+
+
+# View Solution
+
+# Create and name the View (or replace it if it exists)
+CREATE OR REPLACE VIEW Number_of_Titles_View AS
+
+# Create a CTE for the formatted employee names
+WITH cte AS 
+(SELECT 
+	e.emp_no, 
+	CONCAT(e.last_name, ', ', e.first_name) AS full_name
+FROM employees e)
+
+# Select the appropriate fields from the CTE joined on the titles table
+SELECT
+	c.emp_no,
+	c.full_name,
+    	# Count of the unique titles
+	COUNT(DISTINCT t.title) AS title_count
+FROM cte c
+# Inner join the cte on the titles table based on employee number 
+INNER JOIN titles t ON c.emp_no = t.emp_no
+# Add condition for full name, specifically that falls within A and J (inclusive)
+WHERE full_name REGEXP '^[a-j]'
+GROUP BY c.emp_no
+HAVING title_count > 1
+ORDER BY full_name;
+
+# Select from the View
+SELECT 
+	* 
+FROM Number_Of_Titles_View;
+
+
+# Temporary Table Solution
+
+# Create and name the Temporary Table
+CREATE TEMPORARY TABLE Number_Of_Titles_Temp_Table
+
+# Create a CTE for the formatted employee names
+WITH cte AS 
+(SELECT 
+	e.emp_no, 
+	CONCAT(e.last_name, ', ', e.first_name) AS full_name
+FROM employees e)
+
+# Select the appropriate fields from the CTE joined on the titles table
+SELECT
+	c.emp_no,
+	c.full_name,
+	COUNT(DISTINCT t.title) AS title_count
+FROM cte c
+# Inner join the cte on the titles table based on employee number 
+INNER JOIN titles t ON c.emp_no = t.emp_no
+# Add condition for full name, specifically that falls within A and J (inclusive)
+WHERE full_name REGEXP '^[a-j]'
+GROUP BY c.emp_no
+HAVING title_count > 1
+ORDER BY full_name;
+
+# Select from the Temporary Table
+SELECT 
+	* 
+FROM Number_Of_Titles_Temp_Table;
+
+
+# 12) Create both a stored procedure and a user-defined function that calculates
+#	  an employee's average salary, given their employee number. The output should be rounded to 2 decimal places.
+
+
+# Stored Procedure Solution
+
+# Specifiy a new delimiter
+DELIMITER $$
+
+# Specify database
+USE employees $$
+
+# Create procedure with employee number as IN parameter and average salary as OUT parameter
+CREATE PROCEDURE emp_avg_salary_out(IN p_emp_no INTEGER, OUT p_avg_salary DECIMAL(10, 2))
+
+# Create query
+BEGIN
+	SELECT
+		# Specify the average salary as the output
+		AVG(s.salary) INTO p_avg_salary 
+    	FROM employees e
+    	# Join salaries table to employees table on employee ID
+    	JOIN salaries s ON e.emp_no = s.emp_no
+	# Specify the employee number as the IN parameter
+    	WHERE e.emp_no = p_emp_no;
+END$$
+
+# Return the delimiter to ";"
+DELIMITER ;
+
+# Call the procedure with parameters
+CALL emp_avg_salary_out(11300, @p_avg_salary);
+
+SELECT @p_avg_salary;
+
+
+# User-Defined Function Solution
+
+# Specify a new delimiter
+DELIMITER $$
+
+# Create function with employee number as the input parameter and returns the employee's average salary
+CREATE FUNCTION f_emp_avg_salary (p_emp_no INTEGER) RETURNS DECIMAL(10, 2)
+
+# Specify the function as deterministic
+DETERMINISTIC
+
+# Create query
+BEGIN
+
+# Declare the average salary variable
+DECLARE v_avg_salary DECIMAL(10, 2);
+
+	SELECT
+		# Specify the average salary as the output
+		AVG(s.salary) INTO v_avg_salary
+	FROM employees e
+    	# Join salaries table to employees table on employee ID
+	JOIN salaries s ON e.emp_no = s.emp_no
+    	# Specify the employee number as the IN parameter
+   	 WHERE e.emp_no = p_emp_no;
+    	# Specify the return value
+	RETURN v_avg_salary;
+
+END $$
+
+# Return the delimiter to ";"
+DELIMITER ;
+
+# Call the function with parameter
+SELECT f_emp_avg_salary(11300) AS average_salary;
